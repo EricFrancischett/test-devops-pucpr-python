b448e4593997c2da442ba557740571fe4ef98a48
Eric Mello Francischett <101892968+EricFrancischett@users.noreply.github.com>
2025-04-28 20:46:53 -0300

    Update continuousintegration.yml (#12)
    * Update continuousintegration.yml
    
    * fix disc action and random changes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 30000)}
 
 @app.post("/estudantes/cadastro")
 async def create_estudante(estudante: Estudante):
